12e85e0d858fcfc77753054bb08adb34b37c73f2
Oreki1001 <trankimhieu1001@gmail.com>
2025-06-25 23:46:07 +0700

    fix domain

--- src/api/rtkQuery/apiSlice.js
+++ src/api/rtkQuery/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { setCredentials, logout ,setErrorMessage  } from '@/redux/reducer/auth.reducer';
 
 const baseQuery = fetchBaseQuery({ 
-    baseUrl: 'http://localhost:8080',
+    baseUrl: 'https://datn-backend-yr7b.onrender.com',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.login.token;
         if (token) {
